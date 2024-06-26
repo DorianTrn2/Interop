51e67785ef7c701d4f4ea681ee71ea799a475777
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2024-06-26 11:09:34 +0200

    feat: ActiveMQ

---
+++ src/main/java/hr/algebra/dtarin/interop/configuration/ActiveMQConfig.java
@@ -0,0 +1,38 @@
+package hr.algebra.dtarin.interop.configuration;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+@EnableJms
+@Configuration
+public class ActiveMQConfig {
+    String BROKER_URL = "tcp://localhost:61616";
+    String BROKER_USERNAME = "admin";
+    String BROKER_PASSWORD = "admin";
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory(){
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(BROKER_URL);
+        connectionFactory.setPassword(BROKER_USERNAME);
+        connectionFactory.setUserName(BROKER_PASSWORD);
+        return connectionFactory;
+    }
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactory());
+        template.setDefaultDestinationName("queue1");
+        return template;
+    }
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setConcurrency("1-1");
+        return factory;
+    }
+}
--- src/main/java/hr/algebra/dtarin/interop/controller/MainController.java
+++ src/main/java/hr/algebra/dtarin/interop/controller/MainController.java
@@ -1,29 +1,39 @@
 package hr.algebra.dtarin.interop.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
+    private final JmsTemplate jmsTemplate;
+
+    public MainController(JmsTemplate jmsTemplate){
+        this.jmsTemplate = jmsTemplate;
+    }
 
     @GetMapping("/get")
     public ResponseEntity<String> getMethod() {
+        jmsTemplate.convertAndSend( "GET Request");
         return ResponseEntity.ok("GET Request OK");
     }
 
     @PostMapping("/post")
     public ResponseEntity<String> postMethod(@RequestBody String body) {
+        jmsTemplate.convertAndSend( "POST Request");
         return ResponseEntity.ok("POST Request OK");
     }
 
     @PutMapping("/put")
     public ResponseEntity<String> putMethod(@RequestBody String body) {
+        jmsTemplate.convertAndSend( "PUT Request");
         return ResponseEntity.ok("PUT Request OK");
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteMethod() {
+        jmsTemplate.convertAndSend( "DELETE Request");
         return ResponseEntity.ok("DELETE Request OK");
     }
 }
